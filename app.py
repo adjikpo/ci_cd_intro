81ec4bfb4c47a75dc243e77b8fa9f6aa896899f4
adjikpo <arthur.djikpo@gmail.com>
2021-04-01 15:47:39 +0200

    update the travis file

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from flask import Flask
 from markupsafe import escape
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return "Hello world !"
+    return 'Hello world !'
+
 
 @app.route('/hello/<name>')
 def show_user_profile(name):
     return 'Hello %s' % escape(name)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
